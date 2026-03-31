f392b7101c2fe897585e6d777a6faeb497c69290
furkanozkaymakk <74023824+furkanozkaymakk@users.noreply.github.com>
2026-03-31 22:43:53 +0300

    Fix: replace tarifapp package with ytsu, bump versionCode to 3

--- android/app/src/main/java/com/ytsu/MainActivity.kt
+++ android/app/src/main/java/com/ytsu/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tarifapp
+package com.ytsu
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,11 +11,11 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "TarifApp"
+  override fun getMainComponentName(): String = "YTSU"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * which allows you to enable New Architecture with a single boolean flags [googfabricalEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
